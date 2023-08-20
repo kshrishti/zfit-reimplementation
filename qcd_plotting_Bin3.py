b2f74271e8b652b3be6d20936c7992a1b665baab
kshrishti <64732001+kshrishti@users.noreply.github.com>
2023-08-20 13:11:54 -0700

    Update qcd_plotting_Bin3.py

--- qcd_plotting_Bin3.py
+++ qcd_plotting_Bin3.py
@@ -7,11 +7,15 @@ import mplhep
 from zfit import z
 import re
 
-# These values are obtained from `other_workspace_elements.txt` for SRBin3
+# Measuring execution time of zfit fitting
+start = timer()
+
 qcd_bin_vals = [91.9896, 77.4989, 67.9044, 58.9696, 52.7691, 46.4393, 39.324, 33.0533, 26.365, 20.5716, 16.2119, 12.0681, 9.05578, 6.15005, 4.57318, 3.08621, 2.52006]
 qcd_bin_densities = [0.0161797, 0.013631, 0.0119434, 0.0103719, 0.00928135, 0.00816803, 0.00691654, 0.00581361, 0.00463723, 0.00361825, 0.00285144, 0.0021226, 0.00159279, 0.00108171, 0.000804358, 0.00054282, 0]
 
 obs = zfit.Space("msd", limits=(50, 220))
+
+np_data = np.array(qcd_bin_vals)
 
 data_nobin = zfit.Data.from_numpy(obs, array=np_data)
 nbins = 17
@@ -29,11 +33,6 @@ minimum2 = minimizer2.minimize(loss=nll2)
 params = minimum2.params
 print(minimum2)
 
-# Check how good the fitting is
-print(zfit.run(exp_model.pdf(bin_centers)))
-print(qcd_bin_densities)
-print(qcd_bin_vals)
-
 # Plot the exponential fit of the QCD model
 bins = np.linspace(50, 220, 18)
 hist_frequencies = np.array(qcd_bin_vals)
@@ -50,9 +49,18 @@ plt.plot(bin_centers, np.array(qcd_bin_vals).shape[0] / 17 * (220 - 50) * pdf, c
 plt.legend()
 plt.show()
 
+# Check how good the fitting is
+print(zfit.run(exp_model.pdf(bin_centers)))
+print(qcd_bin_densities)
+print(qcd_bin_vals)
+
+
+end = timer()
+print(f'execution time: {end - start}')
+
 
 # FitResult of
-# <UnbinnedNLL model=[<zfit.<class 'zfit.models.basic.Exponential'>  params=[Lambda, Lambda]] data=[<zfit.core.data.Data object at 0x7a1e14060430>] constraints=[]> 
+# <UnbinnedNLL model=[<zfit.<class 'zfit.models.basic.Exponential'>  params=[Lambda, Lambda]] data=[<zfit.core.data.Data object at 0x7fb00ec220b0>] constraints=[]> 
 # with
 # <Minuit Minuit tol=0.001>
 
@@ -66,3 +74,14 @@ plt.show()
 # name      value  (rounded)    at limit
 # ------  ------------------  ----------
 # Lambda          -0.0214455       False
+
+# zfit.run(exp_model.pdf(bin_centers)):
+  # [0.01978129 0.01596313 0.01288194 0.01039548 0.00838896 0.00676973
+  #  0.00546304 0.00440857 0.00355763 0.00287094 0.0023168  0.00186961
+  #  0.00150874 0.00121753 0.00098252 0.00079287 0.00063983]
+# qcd_bin_densities:
+  # [0.0161797, 0.013631, 0.0119434, 0.0103719, 0.00928135, 0.00816803, 0.00691654, 0.00581361, 0.00463723, 0.00361825, 0.00285144, 0.0021226, 0.00159279, 0.00108171, 0.000804358, 0.00054282, 0]
+# qcd_bin_vals:
+  # [91.9896, 77.4989, 67.9044, 58.9696, 52.7691, 46.4393, 39.324, 33.0533, 26.365, 20.5716, 16.2119, 12.0681, 9.05578, 6.15005, 4.57318, 3.08621, 2.52006]
+
+# execution time: 1.3615611370005354
