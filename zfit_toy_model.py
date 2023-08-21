a71feca311871adf2ee738a2603a86fee0487310
kshrishti <64732001+kshrishti@users.noreply.github.com>
2023-08-21 04:51:56 -0700

    Create zfit_toy_model.py

---
+++ zfit_toy_model.py
@@ -0,0 +1,33 @@
+# This would be a continuation of the previous model-building, so we would use the same variables
+
+sampler = exp_model.create_sampler(n=1000, fixed_params=True)
+nll_sample = zfit.loss.UmbinnedNLL(model=exp_model, data=sampler)
+
+minimizer = zfit.minimize.Minuit()
+
+results = []
+nruns = 10
+for run in range(nruns):
+  sampler.resample()  
+  result = minimizer.minimize(nll_sample)
+  results.append(result)
+
+
+# If the parameters are constrained, such as the nuisance parameters that we have in the full model, we would deal with it as follows (with example values for the observation and uncertainty)
+
+observation = 0.022
+uncertainty = 0.001
+
+constraint = zfit.constraint.GaussianConstraing(params=[lam], observation=observation, uncertainty=uncertainty)
+
+n_samples = 5
+
+sampler_new = exp_model.create_sampler(n=n_samples, fixed_params=[lam])
+nll = zfit.loss.UnbinnedNLL(model=exp_model, data=sampler_new, constraints=constraint)
+
+constr_values = constraint.sample(n=n_samples)
+
+for constr_params, constr_vals in constr_values.items():
+    sampler.resample()
+    with zfit.param.set_values(constr_params, constr_vals):
+        minimizer.minimize(nll)  
